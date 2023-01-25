20bb5964ba1fcef1cf4b01f90d904d7186cebb59
HAROUN <haroundominique@gmail.com>
2023-01-25 20:51:46 +0100

    tratando de implementar funcionalidad de cambio de color del panel central por botones, pero salta el mismo error que en el anterior método. Desconozco el motivo

--- src/main/java/com/example/interfazcolores/HelloController.java
+++ src/main/java/com/example/interfazcolores/HelloController.java
@@ -6,6 +6,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -55,21 +57,91 @@ public class HelloController implements Initializable {
     @FXML
     private ScrollBar ScrollBarAzul;
 
+    @FXML
+    private Pane PanelCentral;
+
 
 
     //METODOS
 
-    @FXML
-    private void cambiarColorBoton (ActionEvent event){
+    //TODO: sólo están correctos los códigos para el cambio de color a negro y a rojo, el resto está por codificar
 
+
+    @FXML
+    private void cambiarColorPorBotonNegro (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color:  #000000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonGrisOscuro (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonGris (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonGrisClaro (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonBlanco (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonMagenta (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonAzul (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonCian (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonVerde (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonAmarillo (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonNaranja (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonRojo (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
+    }
+    @FXML
+    private void cambiarColorPorBotonRosa (ActionEvent event){
+
+        PanelCentral.setStyle("-fx-background-color: #ff0000;");
     }
 
+/*
     @FXML
     private void cambiarValorScrollBarRojo(ActionEvent event) {
         double valorScrollBarRojo = ScrollBarRojo.getValue();
         valorRojo.setText(String.valueOf(valorScrollBarRojo));
 
     }
+
+ */
 
 /*
     @FXML
